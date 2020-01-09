465577eff437804eb2e02e0aae0ac4bd44cac469
jonathan Day <jday.day03@gmail.com>
2020-01-09 00:14:36 -0800

    Debugging simple features

--- index.js
+++ index.js
@@ -8,22 +8,30 @@ app.use(express.json())
 
 app.set('view engine','pug')
 app.set('views','./views')
-
+app.enable('strict routing');
 const readDir = util.promisify(fs.readdir)
 
 const homeDirectory = '/home/jonathan/node-projects/home-server'
 
+
+app.get('/folder',(req,res)=> {
+  console.log('folder no slash')
+  res.redirect('/folder/')
+
+
+});
+
 // This is for a folder
-app.get('/folder/?:path(*)',(req,res)=> {
+app.get('/folder/:path(*)',(req,res)=> {
 
 directoryToSearch = path.join(homeDirectory,req.params.path)
 
-
+//http://localhost:3000/
 console.log(`searching directory ${directoryToSearch}`)
-const address ='http://localhost:3000/folder/'
+const address ='folder/'
 // const address ='/folder/'
 
-console.log(`param is ${req.params.path}`)
+console.log(`param is |${ req.params.path.length}|`)
 linkPreface = path.join(address,req.params.path)
 console.log(`preface is ${linkPreface}`)
 
@@ -31,10 +39,13 @@ fs.readdir(directoryToSearch,(err,files)=>{
   if (! files== null){
   files = []
 }
-
+  links = []
+  for (i in files){
+    links.push(path.join(req.params.path,files[i]))
+  }
   console.log(`files is: ${files}` )
-  console.log(`passing these to pug: ${files} and ${linkPreface}`)
-  res.render('fileList',{files : files, linkPreface:linkPreface} )
+  console.log(`passing these to pug: ${links} and ${linkPreface}`)
+  res.render('fileList',{files : links, linkPreface:linkPreface} )
 }
 );
 
