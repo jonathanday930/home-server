d04f79905450ce5759d65694091cef83069481ba
jonathan Day <jday.day03@gmail.com>
2020-01-13 16:30:30 -0800

    Got file walking to work correctly, added file route and started some work on it

--- index.js
+++ index.js
@@ -1,57 +1,18 @@
-const fs = require('fs')
-const util = require('util');
-const path = require('path')
-const express = require('express')
-app = express()
-
-app.use(express.json())
+express= require('express')
+app = express({'strict': true})
 
 app.set('view engine','pug')
 app.set('views','./views')
-app.enable('strict routing');
-const readDir = util.promisify(fs.readdir)
 
-const homeDirectory = '/home/jonathan/node-projects/home-server'
+app.use(express.json({'strict': true}))
+
+var folderRoutes = require('./routes/folder.js')
+app.use('/folder/', folderRoutes);
+
+var fileRoutes = require('./routes/file.js')
+app.use('/file/', fileRoutes);
 
 
-app.get('/folder',(req,res)=> {
-  console.log('folder no slash')
-  res.redirect('/folder/')
-
-
-});
-
-// This is for a folder
-app.get('/folder/:path(*)',(req,res)=> {
-
-directoryToSearch = path.join(homeDirectory,req.params.path)
-
-//http://localhost:3000/
-console.log(`searching directory ${directoryToSearch}`)
-const address ='folder/'
-// const address ='/folder/'
-
-console.log(`param is |${ req.params.path.length}|`)
-linkPreface = path.join(address,req.params.path)
-console.log(`preface is ${linkPreface}`)
-
-fs.readdir(directoryToSearch,(err,files)=>{
-  if (! files== null){
-  files = []
-}
-  links = []
-  for (i in files){
-    links.push(path.join(req.params.path,files[i]))
-  }
-  console.log(`files is: ${files}` )
-  console.log(`passing these to pug: ${links} and ${linkPreface}`)
-  res.render('fileList',{files : links, linkPreface:linkPreface} )
-}
-);
-
-});
-
-console.log(path.join('http://hi', 'b\\c/d'))
 const port = 3000
 // readFolderContents('.')
 app.listen(port,()=> console.log(`listening on ${port}`))
